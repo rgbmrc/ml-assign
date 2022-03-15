2e212ce6728da72d7578e8c96788da229dfff81b
Marco Rigobello <marco.rigobello@hotmail.com>
2022-03-15 20:26:49 +0000

    ✅ post point 2B, 2C runs

--- run_sims.sh
+++ run_sims.sh
@@ -2,4 +2,4 @@
 set -e
 source $HOME/tools/miniconda/etc/profile.d/conda.sh
 conda activate ml
-printf 'n%s\n' {0..24} | shuf | xargs -P4 -i python scripts/train.py 2A {} 2
+printf 'n%s\n' {0..23} | shuf | xargs -P4 -i python scripts/train.py 2C {} 2
